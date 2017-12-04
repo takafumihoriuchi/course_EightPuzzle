9b823b72c97da4529a06de18eb527008e414d5ce
Takafumi Horiuchi <hawkletter080e@icloud.com>
2017-12-05 00:42:53 +0900

    debugging

--- iterative_deepening_search.h
+++ iterative_deepening_search.h
@@ -1,17 +1,26 @@
-// #include "eight_puzzle.h" // needed?
-
-int iterative_deepening_search(int map[][3]);
-
 typedef struct node_open {
 	struct node_open *next;
-	int map_data[][3];
 	int depth;
+	int map_data[][3];
 } OPENSTACK;
 
 typedef struct node_closed {
 	struct node_closed *next;
 	int map_data[][3];
 } CLOSEDLIST;
+
+int iterative_deepening_search(int map[][3]);
+int expand_node(int child[][3], char direction, int child_num, int child_flg[], int map[][3]);
+void push_to_open(int map[][3], int depth);
+int pop_from_open(int map[][3]);
+int get_open_head_depth();
+void insert_to_closed(int map[][3]);
+int is_in_closed(int child[][3]);
+void release_closed_list();
+void copy_array2_to_array1(int array1[][3], int array2[][3]);
+int is_equal_array(int array1[][3], int array2[][3]);
+void check_malloc_open(OPENSTACK *node);
+void check_malloc_closed(CLOSEDLIST *node);
 
 OPENSTACK *open_head = NULL;
 CLOSEDLIST *closed_head = NULL;
@@ -66,57 +75,6 @@ int iterative_deepening_search(int map[][3])
 
 }
 
-void release_closed_list()
-{
-	CLOSEDLIST *tmp;
-	while (closed_head != NULL) {
-		tmp = closed_head;
-		closed_head = (*closed_head).next;
-		free(tmp);
-	}
-	closed_tail = NULL;  // not necessary, but more feels clean to do so
-}
-
-int get_open_head_depth()
-{
-	return (*open_head).depth;
-}
-
-// postcondition : return 1 if given child is in closed list, if not, return 0
-int is_in_closed(int child[][3])
-{
-	CLOSEDLIST *point;
-	point = closed_head;
-	while (1) {
-		if (is_equal_array((*point).map_data, child))
-			return 1;
-		if ((*point).next==NULL)
-			return 0;
-		point = (*point).next;
-	}
-}
-
-// postcondition : return 1 if two arrays are equal, if not, return 0
-int is_equal_array(int array1[][3], int array2[][3])
-{
-	int i, j;
-	for (i=0; i<3; i++)
-		for (j=0; j<3; j++)
-			if (array1[i][j]!=array2[i][j]) return 0;
-	return 1;
-}
-
-void insert_to_closed(int map[][3])
-{
-	CLOSEDLIST *node;
-	node = malloc(sizeof(CLOSEDLIST));
-	check_malloc_closed(node);
-	copy_array2_to_array1((*node).map_data, map);
-	if (closed_head == NULL) closed_tail = node;
-	(*node).next = closed_head;
-	closed_head = node;
-}
-
 // postcondition-1 : if valid, renew the child node, if not, leave the child node as it is
 // postcondition-2 : return 1 if child node is in goal state, return 0 if not
 int expand_node(int child[][3], char direction, int child_num, int child_flg[], int map[][3])
@@ -129,22 +87,6 @@ int expand_node(int child[][3], char direction, int child_num, int child_flg[], 
 		if (is_completed(child)) return 1;
 	}
 	return 0;
-}
-
-void check_malloc_open(OPENSTACK *node)
-{
-	if (node == NULL) {
-		fprintf(stderr, "failed to allocate memory\n");
-		exit(-1);
-	}
-}
-
-void check_malloc_closed(CLOSEDLIST *node)
-{
-	if (node == NULL) {
-		fprintf(stderr, "failed to allocate memory\n");
-		exit(-1);
-	}
 }
 
 void push_to_open(int map[][3], int depth)
@@ -169,10 +111,77 @@ int pop_from_open(int map[][3])
 	return 1;
 }
 
+int get_open_head_depth()
+{
+	return (*open_head).depth;
+}
+
+void insert_to_closed(int map[][3])
+{
+	CLOSEDLIST *node;
+	node = malloc(sizeof(CLOSEDLIST));
+	check_malloc_closed(node);
+	copy_array2_to_array1((*node).map_data, map);
+	if (closed_head == NULL) closed_tail = node;
+	(*node).next = closed_head;
+	closed_head = node;
+}
+
+// postcondition : return 1 if given child is in closed list, if not, return 0
+int is_in_closed(int child[][3])
+{
+	CLOSEDLIST *point;
+	point = closed_head;
+	while (1) {
+		if (is_equal_array((*point).map_data, child))
+			return 1;
+		if ((*point).next==NULL)
+			return 0;
+		point = (*point).next;
+	}
+}
+
+void release_closed_list()
+{
+	CLOSEDLIST *tmp;
+	while (closed_head != NULL) {
+		tmp = closed_head;
+		closed_head = (*closed_head).next;
+		free(tmp);
+	}
+	closed_tail = NULL;  // not necessary, but more feels clean to do so
+}
+
 void copy_array2_to_array1(int array1[][3], int array2[][3])
 {
 	int i, j;
 	for (i=0; i<3; i++)
 		for (j=0; j<3; j++)
 			array1[i][j] = array2[i][j];
+}
+
+// postcondition : return 1 if two arrays are equal, if not, return 0
+int is_equal_array(int array1[][3], int array2[][3])
+{
+	int i, j;
+	for (i=0; i<3; i++)
+		for (j=0; j<3; j++)
+			if (array1[i][j]!=array2[i][j]) return 0;
+	return 1;
+}
+
+void check_malloc_open(OPENSTACK *node)
+{
+	if (node == NULL) {
+		fprintf(stderr, "failed to allocate memory\n");
+		exit(-1);
+	}
+}
+
+void check_malloc_closed(CLOSEDLIST *node)
+{
+	if (node == NULL) {
+		fprintf(stderr, "failed to allocate memory\n");
+		exit(-1);
+	}
 }
